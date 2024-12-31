59fa23fa87125d2ac06f62e7ac35fa44da015e6e
Dzh121 <danielzivharel@email.com>
2024-12-31 20:32:09 +0200

    mamman 14 preproc

---
+++ Mamman 14/assembler.h
@@ -0,0 +1,9 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_LABEL_SIZE 32
+
+#endif //ASSEMBLER_H
---
+++ Mamman 14/funcs.h
@@ -0,0 +1,10 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *handle_malloc(long size);
+int is_reserved (char *line);
+
+#endif
