cf3988dd4088b768155a771481e566393222cce2
Daniel-Desktop <danielzivharel@gmail.com>
2025-03-16 18:54:00 +0200

    Refactor error handling to use stderr for all error and warning messages

--- Mamman 14/src/errors.c
+++ Mamman 14/src/errors.c
@@ -4,7 +4,7 @@
 
 void handle_memory_overflow(char* file_name, int overflow_address)
 {
-	fprintf(stdout, "ERROR: MEMORY OVERFLOW Address: %d out of %d | File: %s\n",
+	fprintf(stderr, "ERROR: MEMORY OVERFLOW Address: %d out of %d | File: %s\n",
 	        overflow_address, MAX_MEMORY_SIZE, file_name);
 }
 
@@ -14,11 +14,11 @@ void print_internal_error(char* error_message)
 	/* Print the internal error message */
 	if (error_message)
 	{
-		fprintf(stdout, "INTERNAL ERROR: %s\n", error_message);
+		fprintf(stderr, "INTERNAL ERROR: %s\n", error_message);
 	}
 	else
 	{
-		fprintf(stdout, "INTERNAL ERROR: Unknown internal error\n");
+		fprintf(stderr, "INTERNAL ERROR: Unknown internal error\n");
 	}
 }
 
@@ -27,23 +27,23 @@ void print_ext_error(char* error_message, char* file_name, int line_number)
 	/* Print the error message with the file name and line number */
 	if (!error_message)
 	{
-		fprintf(stdout, "ERROR: Unknown external error\n"); /* Unknown error */
+		fprintf(stderr, "ERROR: Unknown external error\n"); /* Unknown error */
 		return;
 	}
 
 	if (!file_name)
 	{
-		fprintf(stdout, "ERROR: %s | File: Unknown | Line: %d\n", error_message, line_number); /* Unknown file */
+		fprintf(stderr, "ERROR: %s | File: Unknown | Line: %d\n", error_message, line_number); /* Unknown file */
 		return;
 	}
 
 	if (line_number < 0)
 	{
-		fprintf(stdout, "ERROR: %s | File: %s\n", error_message, file_name); /* No line number */
+		fprintf(stderr, "ERROR: %s | File: %s\n", error_message, file_name); /* No line number */
 	}
 	else
 	{
-		fprintf(stdout, "ERROR: %s | File: %s | Line: %d\n", error_message, file_name, line_number); /* Full details */
+		fprintf(stderr, "ERROR: %s | File: %s | Line: %d\n", error_message, file_name, line_number); /* Full details */
 	}
 }
 
@@ -52,22 +52,22 @@ void print_ext_warning(char* warning_message, char* file_name, int line_number)
 	/* Print the warning message with the file name and line number */
 	if (!warning_message)
 	{
-		fprintf(stdout, "WARNING: Unknown external warning.\n");
+		fprintf(stderr, "WARNING: Unknown external warning.\n");
 		return;
 	}
 
 	if (!file_name)
 	{
-		fprintf(stdout, "WARNING: %s | File: Unknown | Line: %d\n", warning_message, line_number);
+		fprintf(stderr, "WARNING: %s | File: Unknown | Line: %d\n", warning_message, line_number);
 		return;
 	}
 
 	if (line_number < 0)
 	{
-		fprintf(stdout, "WARNING: %s | File: %s\n", warning_message, file_name);
+		fprintf(stderr, "WARNING: %s | File: %s\n", warning_message, file_name);
 	}
 	else
 	{
-		fprintf(stdout, "WARNING: %s | File: %s | Line: %d\n", warning_message, file_name, line_number);
+		fprintf(stderr, "WARNING: %s | File: %s | Line: %d\n", warning_message, file_name, line_number);
 	}
 }
